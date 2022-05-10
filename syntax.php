d918f9d52e5fa0e9bd97535d87d15125af16b13b
gomida <gomida@gmail.com>
2022-05-10 10:54:37 +0000

    code cleanup

--- syntax.php
+++ syntax.php
@@ -27,27 +27,6 @@ class syntax_plugin_sqwall extends DokuWiki_Syntax_Plugin {
         switch ($state) {
             case DOKU_LEXER_ENTER :
                 {
-/*
-                    $m = substr($match,7,1);
-                    if($m == "w"){
-                    $id = substr($match,8,-1);
-                    $file = wikiFN($id);
-                        if(file_exists($file)) {
-                            $fp = fopen($file, "r");
-                            $fr = fread($fp, 4096);
-                            fclose($fp);
-                            if(preg_match('/<sq (.*)>(.*)<\/sq>/',$fr,$match)){
-                            $renderer->doc .= '<div style="position:relative;float:left;width:100%;max-width:720px;margin-bottom:1px;">';
-                            $renderer->doc .= '<a href="'.wl($id).'">';
-                            $renderer->doc .= '<img style="" src="'.ml($match[1],"w=1440,h=720").'" title="'.p_get_first_heading($id).'">';
-                            $renderer->doc .= '<div style="height:100%;width:100%;"><div class="sqwall_title_text">'.p_get_first_heading($id).'</div></div>';
-                            $renderer->doc .= '</a>';
-                            $renderer->doc .= '</div>';
-                            }
-                        }
-                        break;
-                    }
-*/
                     $id = substr($match,8,-1);
                     $file = wikiFN($id);
                     if(file_exists($file)) {
@@ -55,18 +34,20 @@ class syntax_plugin_sqwall extends DokuWiki_Syntax_Plugin {
                         $fr = fread($fp, 4096);
                         fclose($fp);
                         if(preg_match('/<sq (.*)>(.*)<\/sq>/',$fr,$match)){
-                        $renderer->doc .= '<div class="sqwall_box">';
-                        $renderer->doc .= '<a href="'.wl($id).'">';
-                        $renderer->doc .= '<img style="position:absolute;top:0;left:0;" src="'.ml($match[1],'w=360,h=360').'" title="'.p_get_first_heading($id).'">';
-                        $renderer->doc .= '<div class="sqwall_title_box"><div class="sqwall_title_text">'.p_get_first_heading($id).'</div></div>';
-                        $renderer->doc .= '</a>';
-                        $renderer->doc .= '</div>';
+                            // SHOW SQUARE IMAGE
+                            $renderer->doc .= '<div class="sqwall_box">';
+                            $renderer->doc .= '<a href="'.wl($id).'">';
+                            $renderer->doc .= '<img style="position:absolute;top:0;left:0;" src="'.ml($match[1],'w=360,h=360').'" title="'.p_get_first_heading($id).'">';
+                            $renderer->doc .= '<div class="sqwall_title_box"><div class="sqwall_title_text">'.p_get_first_heading($id).'</div></div>';
+                            $renderer->doc .= '</a>';
+                            $renderer->doc .= '</div>';
                         } else {
-                        $renderer->doc .= '<div class="sqwall_box">';
-                        $renderer->doc .= '<a href="'.wl($id).'">';
-                        $renderer->doc .= '<div class="sqwall_title_box" style="visibility:visible;background-color:rgba(0,0,0,0.3);"><div class="sqwall_title_text">'.p_get_first_heading($id).'</div></div>';
-                        $renderer->doc .= '</a>';
-                        $renderer->doc .= '</div>';
+                            // SHOW GRAY SQUARE
+                            $renderer->doc .= '<div class="sqwall_box">';
+                            $renderer->doc .= '<a href="'.wl($id).'">';
+                            $renderer->doc .= '<div class="sqwall_title_box" style="visibility:visible;background-color:rgba(0,0,0,0.3);"><div class="sqwall_title_text">'.p_get_first_heading($id).'</div></div>';
+                            $renderer->doc .= '</a>';
+                            $renderer->doc .= '</div>';
                         }
                     }
 
